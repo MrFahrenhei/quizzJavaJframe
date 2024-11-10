a3ce7a48c829c015c6f3f383ca51996e831b67cb
Vinícius Valle Beraldo <vvberaldo@proton.me>
2024-11-10 16:10:03 -0300

    alterando a main para testar o que é gerado na tela

--- src/main/java/com/app/quizz/main/Main.java
+++ src/main/java/com/app/quizz/main/Main.java
@@ -1,6 +1,8 @@
 package com.app.quizz.main;
 
+import com.app.quizz.models.Category;
 import com.app.quizz.screens.CreateQuestion;
+import com.app.quizz.screens.QuizScreen;
 import com.app.quizz.screens.TitleScreenGui;
 
 import javax.swing.*;
@@ -10,7 +12,8 @@ public class Main {
         SwingUtilities.invokeLater(new Runnable() {
             @Override
             public void run() {
-                new TitleScreenGui().setVisible(true);
+//                new TitleScreenGui().setVisible(true);
+                new QuizScreen(new Category(1, "Javascript"), 10).setVisible(true);
             }
         });
     }
